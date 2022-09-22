3cb7bd51db835fcb1e9b5d02a2c8ce7d34eaf55c
Daniel Xavier <dxavier194@gmail.com>
2022-09-22 15:15:40 -0300

    Update models: Add many subjects has many students relationship

--- src/main/java/com/teachermanagement/daniellucas/models/StudentModel.java
+++ src/main/java/com/teachermanagement/daniellucas/models/StudentModel.java
@@ -1,10 +1,19 @@
 package com.teachermanagement.daniellucas.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @Entity
 @Data
@@ -22,4 +31,6 @@ public class StudentModel {
     private String registration;
     @Column(unique = true)
     private String email;
+    @ManyToMany(mappedBy = "teachers")
+    Set<SubjectModel> subjects = new HashSet<>();
 }
--- src/main/java/com/teachermanagement/daniellucas/models/SubjectModel.java
+++ src/main/java/com/teachermanagement/daniellucas/models/SubjectModel.java
@@ -1,10 +1,22 @@
 package com.teachermanagement.daniellucas.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @Entity
 @Data
@@ -23,4 +35,10 @@ public class SubjectModel {
     @ManyToOne
     @JoinColumn(name = "teacher_id", nullable = false)
     private TeacherModel teacher;
+    @ManyToMany
+    @JoinTable(
+      name = "tb_subject_has_student", 
+      joinColumns = @JoinColumn(name = "subject_id"), 
+      inverseJoinColumns = @JoinColumn(name = "student_id"))
+    private Set<StudentModel> teachers =  new HashSet<>();
 }
