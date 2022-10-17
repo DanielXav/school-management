8e4d1e98766d7ac70f403802f529f43a51e24976
Lucas Lucena <lucas.lucenak@gmail.com>
2022-10-17 16:41:45 -0300

    Update SecurityConfig

--- src/main/java/com/teachermanagement/daniellucas/config/SecurityConfig.java
+++ src/main/java/com/teachermanagement/daniellucas/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.teachermanagement.daniellucas.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -41,6 +46,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.headers().frameOptions().sameOrigin();
 		http.authorizeRequests()
 				.antMatchers(AUTH_WHITELIST).permitAll()
-				.mvcMatchers("/coffees/**").authenticated();
+				.anyRequest().authenticated()
+				.and().addFilter(new AuthenticationFilter(authenticationManager()))
+				.addFilter(new AuthorizationFilter(authenticationManager()))
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**",new CorsConfiguration().applyPermitDefaultValues());
+		return source;
 	}
 }
