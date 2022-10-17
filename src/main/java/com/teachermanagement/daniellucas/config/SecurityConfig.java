01e3616ea2701e081ffe473a8493c68c1fe3817a
Lucas Lucena <lucas.lucenak@gmail.com>
2022-10-17 11:58:07 -0300

    Update PasswordEncoderCongi and SecurityConfig

--- src/main/java/com/teachermanagement/daniellucas/config/SecurityConfig.java
+++ src/main/java/com/teachermanagement/daniellucas/config/SecurityConfig.java
@@ -1,18 +1,34 @@
 package com.teachermanagement.daniellucas.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 	@Bean
-    public UserDetailsService userDetailsService() {
-		return null;
-    }
+	public UserDetailsService userDetailsService() {
+		var userDetailsService = new InMemoryUserDetailsManager();
+
+		var user = User.withUsername("joaozinho")
+				.password(bCryptPasswordEncoder.encode("123456"))
+				.authorities("user")
+				.build();
+
+		userDetailsService.createUser(user);
+
+		return userDetailsService;
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
