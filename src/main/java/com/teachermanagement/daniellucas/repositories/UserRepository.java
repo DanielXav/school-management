7953b57bc864ee815c6bc8c397db78970a823525
Lucas Lucena <lucas.lucenak@gmail.com>
2022-10-17 17:29:36 -0300

    Update project: Refactoring JWT Token

--- src/main/java/com/teachermanagement/daniellucas/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.teachermanagement.daniellucas.repositories;
-
-import com.teachermanagement.daniellucas.models.UserModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserModel, Long> {
-    UserModel findByUsername(String username);
-}
